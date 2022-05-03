7374bfc9ae64e2cda03265d68ce7f5c028c83893
Kishin <kishin@trustless-services.com>
2022-05-03 16:15:08 +0900

    Ready for github pages

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const ejs = require('gulp-ejs');
 const replace = require('gulp-replace');
+const deploy = require('gulp-gh-pages')
 
+// Builds ejs and deploys /dist recursively
 gulp.task('ejs', done => {
   gulp
     .src(["ejs/**/*.ejs", "!" + "ejs/**/_*.ejs"])
     .pipe(ejs({}, {}, {ext:'.html'}))
     .pipe(rename({ extname: ".html" }))
-    .pipe(gulp.dest("./"));
+    .pipe(gulp.dest("./dist"))
+    .pipe(deploy());
   done();
 })
